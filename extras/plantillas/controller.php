babb7b45e7aa84e63a8d40e97a36e27ce2eae55c
Diaz Devera Victor (vitronic) <vitronic2@gmail.com>
2018-09-06 10:38:01 -0400

    Cambios menores

--- extras/plantillas/controller.php
+++ extras/plantillas/controller.php
@@ -1,4 +1,14 @@
 <?php
+/*
+            ____                  _     _
+           / ___|_   _  __ _  ___| |__ (_)
+          | |  _| | | |/ _` |/ __| '_ \| |
+          | |_| | |_| | (_| | (__| | | | |
+           \____|\__,_|\__,_|\___|_| |_|_|
+Copyright (c) 2014  Díaz  Víctor  aka  (Máster Vitronic)
+Copyright (c) 2018  Díaz  Víctor  aka  (Máster Vitronic)
+<vitronic2@gmail.com>   <mastervitronic@vitronic.com.ve>
+*/
 
 class XXX_controller extends controller {
 
--- extras/plantillas/model.php
+++ extras/plantillas/model.php
@@ -1,9 +1,19 @@
 <?php
+/*
+            ____                  _     _
+           / ___|_   _  __ _  ___| |__ (_)
+          | |  _| | | |/ _` |/ __| '_ \| |
+          | |_| | |_| | (_| | (__| | | | |
+           \____|\__,_|\__,_|\___|_| |_|_|
+Copyright (c) 2014  Díaz  Víctor  aka  (Máster Vitronic)
+Copyright (c) 2018  Díaz  Víctor  aka  (Máster Vitronic)
+<vitronic2@gmail.com>   <mastervitronic@vitronic.com.ve>
+*/
 
 class XXX_model extends model {
-        
+
         public function show(){
             print 'Hola Mundo!';
         }
-        
+
 }
