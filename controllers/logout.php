796cb05d4e733019a271f6065a3e2c634303fa3c
Diaz Devera Victor (vitronic) <vitronic2@gmail.com>
2018-09-07 05:48:35 -0400

    Cambiado header HTTP 301 por 307 al salir de la session

--- controllers/logout.php
+++ controllers/logout.php
@@ -6,7 +6,7 @@ class logout_controller extends controller {
     public function execute() {
         if ($this->auth->isLogged()) {
             if($this->auth->logOut()){
-                header("Location: /".start_module, true, 301);
+                header("Location: /".start_module, true, 307);
             }                
         } else {
             print 'Usted no esta logeado  <a href="/">Inicio</a> ';
