4b5fe542f475fbd5a05a637b86a40d47f92cbfd4
Diaz Devera Victor (vitronic) <vitronic2@gmail.com>
2018-09-10 00:11:40 -0400

    Añadida clase API (modelo)  (el controlador se me escapo en el commit anterior)

---
+++ lib/class.api_model.php
@@ -0,0 +1,11 @@
+<?php
+	/* Copyright (c) by Hugo Leisink <hugo@leisink.net>
+	 * This file is part of the Banshee PHP framework
+	 * https://www.banshee-php.org/
+	 *
+	 * Licensed under The MIT License
+	 */
+
+
+	abstract class api_model extends model {
+	}
