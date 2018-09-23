bbe299a4fb2c15bbe737cdce4ed116e4a63936ec
Diaz Devera Victor (vitronic) <vitronic2@gmail.com>
2018-09-23 04:47:07 -0400

    Correccion menor

--- controllers/notFound.php
+++ controllers/notFound.php
@@ -5,7 +5,30 @@
 class notFound_controller extends controller {
 
     public function execute() {
-        $this->model->show();
+        header("Status: 404");
+        /*el titulo del modulo*/
+        $this->view->title          = 'ERROR 404';
+        /*la descripcion del modulo*/
+        $this->view->description    = 'La descripcion';
+        /*el autor del modulo*/
+        $this->view->author         = 'Máster Vitronic';
+        /*seteo todos los parametros de la vista*/
+        $this->view->set();
+        /*La plantilla de la pagina*/
+        $page   = $this->view->tpl->loadTemplate('/public/'.public_theme.'/page');
+        /*En este caso el main va aqui*/
+        $main   = $this->view->tpl->loadTemplate('/public/'.public_theme.'/404');
+        /*Cargo las plantillas en la vista*/
+        $this->view->load($page->render([
+                'metatags'  => $this->view->meta,
+                'main'      => $main->render(),
+                'js'        => $this->view->js
+            ]
+        ));
+        /*en este caso no se requiere cache*/
+        $this->view->cache->cache_on = false;
+        /*escupo el html ya renderizado*/
+        $this->view->generate();
     }
 
 }
--- models/notFound.php
+++ models/notFound.php
@@ -4,31 +4,4 @@
 
 class notFound_model extends model {
 
-    public function show() {
-        header("Status: 404");
-        /*el titulo del modulo*/
-        $this->view->title          = 'ERROR 404';
-        /*la descripcion del modulo*/
-        $this->view->description    = 'La descripcion';
-        /*el autor del modulo*/
-        $this->view->author         = 'Máster Vitronic';
-        /*seteo todos los parametros de la vista*/
-        $this->view->set();
-        /*La plantilla de la pagina*/
-        $page   = $this->view->tpl->loadTemplate('/public/demo/page');
-        /*En este caso el main va aqui*/
-        $main   = $this->view->tpl->loadTemplate('/public/demo/404');
-        /*Cargo las plantillas en la vista*/
-        $this->view->load($page->render([
-                'metatags'  => $this->view->meta,
-                'main'      => $main->render(),
-                'js'        => $this->view->js
-            ]
-        ));
-        /*en este caso no se requiere cache*/
-        $this->view->cache->cache_on = false;
-        /*escupo el html ya renderizado*/
-        $this->view->generate();
-    }
-
 }
